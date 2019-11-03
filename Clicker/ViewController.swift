ed357da86dc6259d940a3693036287306c8ed95f
jimmgrown <jimgrown@yandex.ru>
2019-11-03 01:28:31 -0700

    Initial commit

--- Clicker/ViewController.swift
+++ Clicker/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var iter = 0;
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBAction func incButton(_ sender: Any) {
+        iter = iter + 1
+        nameLabel.text = String(iter)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
